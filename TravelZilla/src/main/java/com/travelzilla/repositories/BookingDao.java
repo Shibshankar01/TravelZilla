2937d17a3d9c619d076350edaade54d56491b2a2
Amrutha010 <karrinagadurgaamrutha040@gmail.com>
2022-12-15 13:41:28 +0530

    added booking and ticketDetails classes

--- TravelZilla/src/main/java/com/travelzilla/repositories/BookingDao.java
+++ TravelZilla/src/main/java/com/travelzilla/repositories/BookingDao.java
@@ -1,5 +1,9 @@
 package com.travelzilla.repositories;
 
-public class BookingDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.travelzilla.models.Booking;
+
+public interface BookingDao extends JpaRepository<Booking,Integer>{
 
 }
--- TravelZilla/src/main/java/com/travelzilla/repositories/TicketDetailsDao.java
+++ TravelZilla/src/main/java/com/travelzilla/repositories/TicketDetailsDao.java
@@ -1,5 +1,9 @@
 package com.travelzilla.repositories;
 
-public class TicketDetailsDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.travelzilla.models.TicketDetails;
+
+public interface TicketDetailsDao extends JpaRepository<TicketDetails,Integer>{
 
 }
