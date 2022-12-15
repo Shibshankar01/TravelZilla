8a67f64640d6f1d2da665c8c3e87bae116b2bea5
Ajay470 <105915458+Ajay470@users.noreply.github.com>
2022-12-15 14:36:54 +0530

    new chan

--- TravelZilla/src/main/java/com/travelzilla/models/Package.java
+++ TravelZilla/src/main/java/com/travelzilla/models/Package.java
@@ -1,7 +1,14 @@
-package com.travelzilla.models
+package com.travelzilla.models;
 
 	
 
+import java.util.Objects;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Min;
