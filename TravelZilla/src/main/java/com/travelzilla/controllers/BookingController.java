5cbe1e99727875fa04a926eafb1db793f2e5f788
Amrutha010 <karrinagadurgaamrutha040@gmail.com>
2022-12-18 18:10:47 +0530

    added validity

--- TravelZilla/src/main/java/com/travelzilla/controllers/BookingController.java
+++ TravelZilla/src/main/java/com/travelzilla/controllers/BookingController.java
@@ -13,12 +13,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.travelzilla.exceptions.AdminException;
 import com.travelzilla.exceptions.BookingException;
+import com.travelzilla.exceptions.SessionException;
+import com.travelzilla.models.Admin;
 import com.travelzilla.models.Booking;
 import com.travelzilla.models.BookingDTO;
+import com.travelzilla.models.Report;
+import com.travelzilla.models.Session;
+import com.travelzilla.models.UserType;
 import com.travelzilla.services.BookingServices;
+import com.travelzilla.services.SessionServices;
 
 @RestController
 @RequestMapping("/booking")
@@ -26,24 +34,48 @@ public class BookingController {
 
 	@Autowired
 	private BookingServices bService;
+	
+	@Autowired
+	private SessionServices sServices;
 
 	@PostMapping("/makeBooking")
-	public ResponseEntity<Booking> makeBooking(@Valid @RequestBody BookingDTO booking) throws BookingException {
-		return new ResponseEntity<Booking>(bService.makeBooking(booking), HttpStatus.OK);
+	public ResponseEntity<Booking> makeBooking(@Valid @RequestBody BookingDTO booking, @RequestParam("sessionKey")String sessionKey) throws BookingException, SessionException {
+		Session session = sServices.getASessionByKey(sessionKey);
+		if(session.getUserId()==booking.getCustomerId()&& session.getUserType()==UserType.CUSTOMER) {
+			return new ResponseEntity<Booking>(bService.makeBooking(booking), HttpStatus.OK);
+		}
+		throw new BookingException("Please login with the correct credentials");
 	}
 
 	@DeleteMapping("/cancelBooking/{id}")
-	public ResponseEntity<Booking> deleteBookingById(@PathVariable("id") Integer bookingId) throws BookingException {
-		return new ResponseEntity<Booking>(bService.cancelBookingById(bookingId), HttpStatus.OK);
+	public ResponseEntity<Booking> deleteBookingById(@PathVariable("id") Integer bookingId, @RequestParam("sessionKey")String sessionKey,@RequestParam("customerId") int customerId) throws BookingException, SessionException {
+		Session session = sServices.getASessionByKey(sessionKey);
+		if(session.getUserId()==customerId&&session.getUserType()==UserType.CUSTOMER) {
+			Booking booking = bService.cancelBookingById(bookingId);
+			return new ResponseEntity<Booking>(bService.cancelBookingById(bookingId), HttpStatus.OK);
+		}
+		throw new BookingException("Please login with the correct credentials");
+		
 	}
 
 	@GetMapping("/viewBooking/{id}")
-	public ResponseEntity<Booking> viewBookingById(@PathVariable("id") Integer bookingId) throws BookingException {
-		return new ResponseEntity<Booking>(bService.ViewBookingById(bookingId), HttpStatus.OK);
+	public ResponseEntity<Booking> viewBookingById(@PathVariable("id") Integer bookingId, @RequestParam("sessionKey")String sessionKey,@RequestParam("customerId") int customerId) throws BookingException, SessionException {
+		Session session = sServices.getASessionByKey(sessionKey);
+		if(session.getUserId()==customerId&& session.getUserType()==UserType.CUSTOMER) {
+			Booking booking = bService.cancelBookingById(bookingId);
+			return new ResponseEntity<Booking>(bService.ViewBookingById(bookingId), HttpStatus.OK);
+		}
+		throw new BookingException("Please login with the correct credentials");
+		
 	}
 
 	@GetMapping("/viewAllBookings")
-	public ResponseEntity<List<Booking>> viewAllBookings() throws BookingException {
-		return new ResponseEntity<List<Booking>>(bService.viewAllBookings(), HttpStatus.OK);
+	public ResponseEntity<List<Booking>> viewAllBookings( @RequestParam("sessionKey")String sessionKey ) throws BookingException, SessionException {
+		Session session = sServices.getASessionByKey(sessionKey);
+		if(session.getUserType()==UserType.ADMIN) {
+			return new ResponseEntity<List<Booking>>(bService.viewAllBookings(), HttpStatus.OK);
+		}
+		throw new BookingException("Please login with the correct credentials");
+		
 	}
 }
