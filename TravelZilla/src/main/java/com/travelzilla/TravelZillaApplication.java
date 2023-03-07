b81e02e0e687d3a122957a3d8e7a910b9a044ecd
Shibshankar Padhy <shivapadhy547@gmail.com>
2023-03-07 13:27:50 +0530

    Changes in Project file to avoid CORS error

--- TravelZilla/src/main/java/com/travelzilla/TravelZillaApplication.java
+++ TravelZilla/src/main/java/com/travelzilla/TravelZillaApplication.java
@@ -3,7 +3,12 @@ package com.travelzilla;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+servers = {@Server(url = "/", description = "Default Server URL")})
 public class TravelZillaApplication {
 
 	public static void main(String[] args) {
